c82d096eeb039c9da44c02b192cb182c27b52156
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2025-08-20 14:26:14 +0530

    lec6 1

---
+++ Web/Node/lec6/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Welcome to root Route!');
+});
+
+app.get('/human', (req, res) => {
+    res.send('Welcome to Human Route!');
+});
+
+app.get('/blogs', (req, res) => {
+    res.render('blogs/index', { blogs });
+});
+
+app.get('/blogs/new', (req, res) => {
+    res.render('blogs/new');
+});
+
+app.post('/blogs', (req, res) => {
+    let { author, comment } = req.body;
+    blogs.push({ id: blogs.length + 1 ,author, comment });
+    res.send("Blog created successfully!");
+});
+
+let password = 212212;
+app.use('/home',(req,res,next)=>{
+    if(password === 212212){
+        next();
+    }
+    else{
+        res.render("home");
+    }
+})
+
+// CRUD
+// Create, Read, Update, Delete
+
+const blogs = [
+    {
+        id: 1,
+        author: 'Ujjwal',
+        comment: "Aaj kamaenga to kl bathke khaenge",
+    },
+    {
+        id: 2,
+        author: 'Arpit',
+        comment: "Aaj khaenge to kl kamaenge",
+    },
+    {
+        id: 3,
+        author: 'Anshul',
+        comment: "Aaj khaenge to kl khaenge",
+    },
+    {
+        id: 4,
+        author: 'Aryan',
+        comment: "Papa woh cycle ki toh....",
+    }
+]
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
