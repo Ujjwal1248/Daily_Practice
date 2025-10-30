986b23f3d2bd7a070f01669b009d6ab97fca2496
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2025-10-30 15:31:09 +0530

    swiggy_02

---
+++ Web/React/swiggy_02/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Body from './components/Body'
+import Footer from './components/Footer'
+import Apicalling from './components/Apicalling'
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Body/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
