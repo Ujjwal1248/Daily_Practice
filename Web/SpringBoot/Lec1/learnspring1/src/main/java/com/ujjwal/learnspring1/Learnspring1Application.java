6190a78f16c076096cbec74aec42dd819fa69fda
Ujjwal Agrawal <144346166+Ujjwal1248@users.noreply.github.com>
2026-01-28 15:05:33 +0530

    Springboot-lec1

---
+++ Web/SpringBoot/Lec1/learnspring1/src/main/java/com/ujjwal/learnspring1/Learnspring1Application.java
@@ -0,0 +1,13 @@
+package com.ujjwal.learnspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learnspring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learnspring1Application.class, args);
+	}
+
+}
